513d1ea26fb5060b6aa7fcf923fba67044c7b305
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-01-18 15:56:15 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'usefulltools',
   author_email = 'example@example.com',
-  version = '0.1.4',
+  version = '0.1.5',
   classifiers = [
       "Programming Language :: Python :: 3",
   ],
